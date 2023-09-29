2195499573de73154d0ef9eb08bd65075df4d8e4
Emily Lister <100031616@ccisd.net>
2023-09-29 10:52:34 -0500

    testing commital

--- HillCipherRunner.java
+++ HillCipherRunner.java
@@ -12,13 +12,19 @@ public class HillCipherRunner
     {
 
         Scanner kb = new Scanner(System.in);
-        kb.next();
+
         HillCipher userCipher;
         System.out.println("Welcome to Hill Cipher encryption and decryption! Do you have a key for your encryption? (y/n)");
         if (kb.next().toLowerCase().equals("n")){
             System.out.println("No worries! We'll give you one. Would you like the key to be with a matrix size 2 or 3? (2/3)");
-            
-        } else if (kb.next().toLowerCase().equals("y"))
+            int n = kb.nextInt();
+            System.out.println("What message would you like to encrypt/decrypt?");
+            kb.next();
+            String message = kb.nextLine();
+
+            userCipher = new HillCipher(message, n);
+
+        } else
         {
             System.out.println("Perfect! This program only takes matrix of size 2 or 3. Which size is your key? (2/3)");
             if (kb.nextInt() == 2){
@@ -30,11 +36,11 @@ public class HillCipherRunner
                     }
                 }
                 System.out.println("What message would you like to encrypt/decrypt?");
-                userCipher = new HillCipher(kb.next(), temp);
+                userCipher = new HillCipher(kb.nextLine(), temp);
 
 
 
-            } else if (kb.nextInt() == 3){
+            } else {
                 int[][] temp = new int[3][3];
                 for (int row = 0; row< 3; row++){
                     for (int col = 0; col < 3; col++){
@@ -46,8 +52,14 @@ public class HillCipherRunner
                 userCipher = new HillCipher(kb.nextLine(), temp);
             }
 
-
         }
+        System.out.println("Would you like to encrypt or decrypt? (e/d)");
+        if (kb.next().toLowerCase().equals("e")){
+            System.out.println("Your encrypted message is : " + userCipher.encrypt());
+        } else if (kb.next().toLowerCase().equals("d")){
+            System.out.println("Your encrypted message is : " + userCipher.decrypt());
+        }
+
 
     }
 
